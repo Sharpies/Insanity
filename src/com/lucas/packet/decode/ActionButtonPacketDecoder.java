736c5332712b79beca8cad3be010acb8bb03cee3
Lucas <smokin2005@gmail.com>
2013-08-30 21:59:59 -0500

    Updated some naming and added debug

--- src/com/lucas/packet/decode/ActionButtonPacketDecoder.java
+++ src/com/lucas/packet/decode/ActionButtonPacketDecoder.java
@@ -1,9 +1,12 @@
 package com.lucas.packet.decode;
 
+import java.util.logging.Logger;
+
 import server.model.players.Client;
 
 import com.lucas.packet.IPacketMessageDecoder;
 import com.lucas.packet.Packet;
+import com.lucas.packet.encode.LogOutPacketEncoder;
 
 /**
  * The action button packet decoder using the implementation of {@link IPacketMessageDecoder}
@@ -12,20 +15,30 @@ import com.lucas.packet.Packet;
  */
 public final class ActionButtonPacketDecoder implements IPacketMessageDecoder {
 
+	/**
+	 * The {@link Logger} instance for the {@link ActionButtonPacketDecoder} class.
+	 */
+	private static Logger logger = Logger.getLogger(ActionButtonPacketDecoder.class.getName());
+	
 	/*
 	 * (non-Javadoc)
 	 * @see com.lucas.packet.IPacketMessageDecoder#decodePacket(server.model.players.Client, com.lucas.packet.Packet)
 	 */
 	@Override
 	public void decodePacket(Client player, Packet packet) {
-		short actionButton = packet.getShort();
-		switch(actionButton) {
+		final int button = packet.getShort();
+		switch(button) {
 		
 		/*
 		 * The logout button handler
 		 */
 		case 2458:
-			
+			player.writePacketMessage(new LogOutPacketEncoder());
+			break;
+		
+		default:
+			player.sendMessage("Un supported button id:" +button);
+			logger.info("Unhandled action button : " + button);
 			break;
 		}
 	}
